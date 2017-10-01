87c28fea936b5748f0d7807365433cb78453741a
Trenton Ford <tford9@lsu.edu>
2017-10-01 16:37:55 -0400

    adding Doxygen file headers to utilities.* files

--- utilities.cpp
+++ utilities.cpp
@@ -1,4 +1,13 @@
+/**
+ * @file utilties.cpp
+ * @author Trenton Ford
+ * @date 28 Sep 2017
+ * @brief Source for utilities.* 
+ *
+ * Procedures, functions, and datatypes used in multiple source code files.
+ */
+
 void print_results()
 {
-	
+
 }
---
+++ utilities.hpp
@@ -0,0 +1,8 @@
+/**
+ * @file utilties.hpp
+ * @author Trenton Ford
+ * @date 28 Sep 2017
+ * @brief Source for utilities.* 
+ *
+ * Procedures, functions, and datatypes used in multiple source code files.
+ */
