c364d96344ffd25d6b0c04499afc97cab1b87853
Trenton Ford <tford9@lsu.edu>
2017-10-23 20:53:19 -0400

    add working version of data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -1,18 +1,30 @@
 # Author: Trenton W. Ford
 # Date: 10/23/2017
-# Purpose:
-import subprocess
-from subprocess import Popen, PIPE
+# Purpose: Automate simulator calls and record results.
 
-p = Popen("", stdin=PIPE,stdout=PIPE)   # set environment, start new shell
-p.communicate("./sim")
-p.communicate("64")
-p.communicate("8")
-p.communicate("1")
-p.communicate("2")
-p.communicate("2")
-p.communicate("/edgelists/g6-36692,367662")
+from subprocess import Popen, PIPE, STDOUT
 
+TravAlgs = [1,2];
+ReplPol= [1,2,3];
+DatForms = [1,2];
+
+CacheSizesKB = [8, 64, 128, 256, 512, 1024]
+PageSizesKB = [2,4,8,16,32,64]
+
+for cs in CacheSizesKB :
+    string = "";
+    string += str(cs) + '\n';
+    for ps in PageSizesKB :
+        string1="";
+        string1 +=string + str(ps) + '\n';
+        for rp in ReplPol:
+            string2="";
+            string2 +=string1 + "1\n" + str(rp) +"\n2\n/home/tford/Repositories/ND_Research/Grad-OS/edgelists/g1\n"
+
+            p = Popen(['./sim', ''], stdout=PIPE, stdin=PIPE, stderr=STDOUT) 
+            sim_stdout = p.communicate(input=bytes(string2, encoding='ascii'))[0]
+            print(sim_stdout.decode())
+            #print(string2);
 
 
 
