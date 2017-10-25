6b05dbc2183377d012bd712ba69b3480f807f7da
Trenton Ford <tford9@lsu.edu>
2017-10-25 14:34:38 -0400

    add cache and page variations

--- data_gen.py
+++ data_gen.py
@@ -1,24 +1,26 @@
 # Author: Trenton W. Ford
 # Date: 10/23/2017
 # Purpose: Automate simulator calls and record results.
-
+import numpy as np
 from subprocess import Popen, PIPE, STDOUT
 
 TravAlgs = [1,2];
 DatForms = [1,2];
 ReplPol= [1,2,3];
 
-CacheSizesKB = [8, 64, 128, 256, 512, 1024]
-PageSizesKB = [2,4,8,16,32,64]
+CacheSizesKB = np.arange(8,13,2);
+
+print(CacheSizesKB)
 
 output_file = open('output.txt', 'w')
 
 for cs in CacheSizesKB :
     string = "";
-    string += str(cs) + '\n';
-    for ps in PageSizesKB :
+    string += str(2**cs) + '\n';
+    PageSizesKB = np.arange(cs-7, cs-2,2);
+    for ps in PageSizesKB:
         string1="";
-        string1 +=string + str(ps) + '\n';
+        string1 +=string + str(2**ps) + '\n';
         for ta in TravAlgs:
             string2 = "";
             string2 +=string1 + str(ta) + '\n';
@@ -29,9 +31,9 @@ for cs in CacheSizesKB :
                     string4="";
                     string4 +=string3 + str(rp) + '\n' + "./edgelists/g2\n";
 
-                    p = Popen(['./sim', ''], stdout=PIPE, stdin=PIPE, stderr=PIPE) 
-                    sim_stdout, sim_stderr = p.communicate(input=bytes(string4, encoding='ascii'))
-                    output_file.write(sim_stdout.decode());
+                    # p = Popen(['./sim', ''], stdout=PIPE, stdin=PIPE, stderr=PIPE) 
+                    # sim_stdout, sim_stderr = p.communicate(input=bytes(string4, encoding='ascii'))
+                    # output_file.write(sim_stdout.decode());
                     print(string4);
 
 
