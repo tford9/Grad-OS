b9351f12fe76dd93ee5a01e3d51b9ab1b68f7f26
Trenton Ford <tford9@lsu.edu>
2017-10-25 13:43:15 -0400

    updating python script

--- data_gen.py
+++ data_gen.py
@@ -19,12 +19,18 @@ for cs in CacheSizesKB :
     for ps in PageSizesKB :
         string1="";
         string1 +=string + str(ps) + '\n';
-        for rp in ReplPol:
-            string2="";
-            string2 +=string1 + "1\n" + str(rp) +"\n2\n./edgelists/g1\n"
+        for ta in TravAlgs:
+            string2 = "";
+            string2 +=string1 + str(ta) + '\n';
+            for df in DatForms:
+                string3 = "";
+                string3 = string2 + str(df) + '\n';
+                for rp in ReplPol:
+                    string4="";
+                    string4 +=string3 + str(rp) + '\n' + "./edgelists/g2\n";
 
             p = Popen(['./sim', ''], stdout=PIPE, stdin=PIPE, stderr=PIPE) 
-            sim_stdout, sim_stderr = p.communicate(input=bytes(string2, encoding='ascii'))
+            sim_stdout, sim_stderr = p.communicate(input=bytes(string4, encoding='ascii'))
             output_file.write(sim_stdout.decode());
             # print(string2);
 
